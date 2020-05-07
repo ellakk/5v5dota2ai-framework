05cf78a9f215e5c4fda74c563d878c5008e26326
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-05-07 11:10:45 +0200

    Add new ability casting system

--- src/game/PlayerHero.py
+++ src/game/PlayerHero.py
@@ -90,3 +90,89 @@ class PlayerHero(Hero):
 
     def noop(self):
         self.command = {self.getName(): {"command": "NOOP"}}
+
+    def cast_toggle(self, abilityIndex):
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_TOGGLE",
+                "ability": abilityIndex,
+            }
+        }
+
+    def cast_no_target(self, abilityIndex):
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_NO_TARGET",
+                "ability": abilityIndex,
+            }
+        }
+
+    def cast_target_point(self, abilityIndex, position):
+        x, y, z = position
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_NO_TARGET",
+                "ability": abilityIndex,
+                "x": x,
+                "y": y,
+                "z": z
+            }
+        }
+
+    def cast_target_area(self, abilityIndex, position):
+        x, y, z = position
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_TARGET_AREA",
+                "ability": abilityIndex,
+                "x": x,
+                "y": y,
+                "z": z
+            }
+        }
+
+    def cast_target_unit(self, abilityIndex, target):
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_TARGET_UNIT",
+                "ability": abilityIndex,
+                "target": target
+            }
+        }
+
+    def cast_vector_targeting(self, abilityIndex, position):
+        x, y, z = position
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_VECTOR_TARGETING",
+                "ability": abilityIndex,
+                "x": x,
+                "y": y,
+                "z": z
+            }
+        }
+
+    def cast_target_unit_aoe(self, abilityIndex, target):
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_TARGET_UNIT_AOE",
+                "ability": abilityIndex,
+                "target": target
+            }
+        }
+
+    def cast_combo_target_point_unit(self,
+                                     abilityIndex,
+                                     target=-1,
+                                     position=[-1, -1, -1]):
+        x, y, z = position
+        self.command = {
+            self.getName(): {
+                "command": "CAST_ABILITY_TARGET_COMBO_TARGET_POINT_UNIT",
+                "ability": abilityIndex,
+                "target": target,
+                "x": x,
+                "y": y,
+                "z": z
+            }
+        }
